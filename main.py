dc2e36a0cffbc4410ca11dd91b418524ce66602a
Mitmit101 <132913154+Mitmit101@users.noreply.github.com>
2024-12-18 18:58:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ df = data.frame
 df.sample(5)
 df.dtypes
 features = list(df.columns)
-print("Available features:", features)
-selected_features = [features[0], features[2]]
-print("Selected features: ", selected_features)
+selected_features = [features[0], features[2],features[3], features[7], features[8]]
+
 fig, axs  = plt.subplots(1, len(selected_features), figsize = (20,3))
 for ax, f in zip(axs, selected_features):
     ax.hist(df[f], bins=5, color='skyblue', edgecolor='black')
     ax.set_xlabel(f)
+    
 reference_feature = selected_features[1]
 y = df[reference_feature]
 
